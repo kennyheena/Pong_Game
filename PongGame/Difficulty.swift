3f13ac43fdc112fd22465f65d928b15b5fa236e7
Ashleigh Pinch <pinch_01@ashleighs-mac-mini.local>
2017-04-23 16:32:46 +1000

    Updated Project Files, added logic for win losses

---
+++ PongGame/Difficulty.swift
@@ -0,0 +1,17 @@
+//
+//  Difficulty.swift
+//  PongGame
+//
+//  Created by Ashleigh Pinch on 23/04/2017.
+//  Copyright © 2017 Sandun Heenatigala. All rights reserved.
+//
+
+import Foundation
+
+public class Difficulty
+{
+    static var Easy = 0.4
+    static var Medium = 0.2
+    static var Hard = 0.1
+    
+}
--- PongGame/GameScene.swift
+++ PongGame/GameScene.swift
@@ -3,6 +3,7 @@
 //  PongGame
 //
 //  Created by Sandun Heenatigala on 9/4/17.
+//  Updated by Sandun Heenatigala on 23/4/17
 //  Copyright © 2017 Sandun Heenatigala. All rights reserved.
 //
 
@@ -11,79 +12,87 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //making three variables for 3 objects (paddle1, paddle2 and ball)
+    var paddle1 = SKSpriteNode ()
+    var paddle2 = SKSpriteNode ()
+    var ball = SKSpriteNode ()
+    public var difficulty = Difficulty.Hard
+    public var score = [Int]()
+    
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //assigning variables for the sprites
+        paddle1 = self.childNode(withName: "paddle1") as! SKSpriteNode
+        paddle2 = self.childNode(withName: "paddle2") as! SKSpriteNode
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+    
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //ball bounce angle and ball speed
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            paddle1.run(SKAction.moveTo(x: location.x , duration: 0.1))
+            //paddle2.run(SKAction.moveTo(x: location.y , duration: 0.1))
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            paddle1.run(SKAction.moveTo(x: location.x , duration: 0.1))
+            //paddle2.run(SKAction.moveTo(x: location.y , duration: 0.1))
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func addScore(winner: SKSpriteNode)
+    {
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        paddle1.position = CGPoint(x: 0 , y: paddle1.position.y)
+        paddle2.position = CGPoint(x: 0 , y: paddle2.position.y)
+        
+        if (winner == paddle1)
+        {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        }
+        else if (winner == paddle2)
+        {
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        //paddle2 slider movement when the ball travel (paddle 2 is tracking the ball)
+        paddle2.run(SKAction.moveTo(x: ball.position.x, duration: difficulty))
+
+        
+        //adding checks for when ball reaches bottom or top
+        if (ball.position.y <= paddle1.position.y - 50)
+        {
+            addScore(winner: paddle2)
+        }
+        else if(ball.position.y >= paddle2.position.y + 50)
+        {
+            addScore(winner: paddle1)
+        }
     }
 }
